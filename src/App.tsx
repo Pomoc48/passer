c68d59a6287cc4ed4f2e1a670bcde1f57267c3eb
Pomoc48 <lukawski.mikolaj@gmail.com>
2023-02-08 21:17:13 +0100

    Add firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { initializeApp } from "firebase/app";
+import Login from './pages/Login';
 
 function App() {
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyC6v6N8InH2SNyjoGnfgQ_DPmV2Xw30f4k",
+    authDomain: "pass-9f64b.firebaseapp.com",
+    projectId: "pass-9f64b",
+    storageBucket: "pass-9f64b.appspot.com",
+    messagingSenderId: "31474752244",
+    appId: "1:31474752244:web:68a51225edf4bc9786dca1",
+    measurementId: "G-S9TYFLCXWG"
+  };
+
+  const app = initializeApp(firebaseConfig);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Login app={app} />
   );
 }
 
