6d5a12d74bca342d0f0f8f2fdbd84e181e71f92a
Mikołaj Łukawski <lukawski.mikolaj@gmail.com>
2023-04-25 15:44:41 +0200

    Add encrypted data type

--- src/types/SiteData.tsx
+++ src/types/SiteData.tsx
@@ -1,8 +1,10 @@
+import { EncryptedData } from "./EncryptedData";
+
 export type SiteData = {
     uuid: string;
-    name: string;
-    note: string;
-    password: string;
-    username: string;
-    url: string;
+    name: EncryptedData;
+    note: EncryptedData;
+    password: EncryptedData;
+    username: EncryptedData;
+    url: EncryptedData;
 }
