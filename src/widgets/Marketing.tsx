ff7cfa59f0649648ac30ca899abcab9b6837204a
Mikołaj Łukawski <lukawski.mikolaj@gmail.com>
2023-04-25 15:46:55 +0200

    Add padlock render image

--- src/widgets/Marketing.tsx
+++ src/widgets/Marketing.tsx
@@ -1,11 +1,15 @@
 import '../scss/widgets/Marketing.scss';
+import Padlock from '../media/padlock.png';
 
 export default function Marketing() {
   return (
     <div className="Marketing">
-      <div className='Title'>Open-source password manager</div>
-      <div className='Subtitle'>presented by Mikołaj Łukawski</div>
-      <div className='Info'>Your passwords are client-side encrypted before being sent to the Firebase Firestore database</div>
+      <img src={Padlock} alt="Padlock" />
+      <div className="Texts">
+        <div className='Title'>Open-source password manager</div>
+        <div className='Subtitle'>presented by Mikołaj Łukawski</div>
+        <div className='Info'>Your passwords are client-side encrypted before being sent to the Firebase Firestore database.</div>
+      </div>
     </div>
   );
 }
