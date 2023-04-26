c0272faa1e02a786b823eba862e32ce083b2362d
Mikołaj Łukawski <lukawski.mikolaj@gmail.com>
2023-04-26 14:06:46 +0200

    Update page text

--- src/widgets/Marketing.tsx
+++ src/widgets/Marketing.tsx
@@ -1,15 +1,33 @@
 import '../scss/widgets/Marketing.scss';
 import Padlock from '../media/padlock.png';
+import Key from '../media/key.png';
 
 export default function Marketing() {
   return (
     <div className="Marketing">
-      <img src={Padlock} alt="Padlock" />
-      <div className="Texts">
-        <div className='Title'>Open-source password manager</div>
-        <div className='Subtitle'>presented by Mikołaj Łukawski</div>
-        <div className='Info'>Your passwords and site details are client-side encrypted. Everything can be self-hosted.</div>
-      </div>
+      <section>
+        <img src={Padlock} alt="Padlock" />
+        <div className="Texts">
+          <div className='Title'>Passer</div>
+          <div className='Info'>Open-source password manager, presented by Mikołaj Łukawski. The solution for all your password storing related problems.</div>
+        </div>
+      </section>
+      <div className="separator"/>
+      <section>
+        <div className="Texts">
+          <div className='Title'>Secure</div>
+          <div className='Info'>Your passwords and all site data are client-side encrypted, using your master password, before being stored on the database.</div>
+        </div>
+        <img src={Key} alt="Key" />
+      </section>
+      <div className="separator"/>
+      <section>
+        <img src={Padlock} alt="Key" />
+        <div className="Texts">
+          <div className='Title'>Transparent</div>
+          <div className='Info'>Source code is fully available in the GitHub repo, and the entire solution is easily self-hostable using the free tier of Firebase.</div>
+        </div>
+      </section>
     </div>
   );
 }
