521a293f4d44905d25cf9ec457d3a08f9dd7a4a8
Mikołaj Łukawski <lukawski.mikolaj@gmail.com>
2023-05-23 18:59:39 +0200

    Replace render images with icons

--- src/pages/welcome/index.tsx
+++ src/pages/welcome/index.tsx
@@ -1,7 +1,4 @@
 import './style.css';
-import Padlock from '../../assets/padlock.png';
-import Key from '../../assets/key.png';
-import Magnify from '../../assets/magnify.png';
 import Navbar from '../../components/navbar';
 import GoogleSignIn from '../../components/google-login';
 
@@ -13,7 +10,7 @@ export default function WelcomePage() {
     <main>
       <div className="section-container">
         <section>
-          <img src={Padlock} alt="Padlock" />
+          <span className="material-icons">password</span>
           <div className="text">
             <h2 className='primary-text display-large'>Passer</h2>
             <p className='body-large'>Open-source password manager, presented by Mikołaj Łukawski. The solution for all your password storing related problems.</p>
@@ -26,12 +23,12 @@ export default function WelcomePage() {
             <h3 className='primary-text headline-large'>Secure</h3>
             <p className='body-large'>Your passwords and all site data are client-side encrypted, before being safely stored on the database. The encryption/decryption key is generated locally using your master password and never leaves your device.</p>
           </div>
-          <img src={Key} alt="Key" />
+          <span className="material-icons">lock_outline</span>
         </section>
       </div>
       <div className="section-container">
         <section>
-          <img src={Magnify} alt="Key" />
+          <span className="material-icons">code</span>
           <div className="text">
             <h3 className='primary-text headline-large'>Transparent</h3>
             <p className='body-large'>The source code is fully available with it's documentation and step by step instructions in the GitHub repo, and the entire solution is easily self-hostable using the free tier of Firebase.</p>
