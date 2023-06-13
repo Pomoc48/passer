9dce3995368421b17cc149d18aa6fa755a2372cf
Mikołaj Łukawski <lukawski.mikolaj@gmail.com>
2023-06-13 15:42:39 +0200

    Add dialog fade animation

--- src/components/dialog/index.tsx
+++ src/components/dialog/index.tsx
@@ -1,9 +1,26 @@
+import { useEffect } from 'react';
 import { DialogParameters } from '../../types/dialogParameters';
 import './style.css';
 
 export default function MaterialDialog(params: DialogParameters) {
+  useEffect(() => {
+    setTimeout(() => {
+      document.getElementsByClassName("dialog")[0].classList.add("open");
+      document.getElementsByClassName("scrim")[0].classList.add("open");
+    }, 1);
+  }, []);
+
   return <>
-    <div className="scrim" onClick={params.closeFunction} />
+    <div
+      className="scrim"
+      onClick={() => {
+        document.getElementsByClassName("dialog")[0].classList.remove("open");
+        document.getElementsByClassName("scrim")[0].classList.remove("open");
+        setTimeout(() => {
+          params.closeFunction();
+        }, 300);
+      }}
+    />
     <div className="dialog">
       <h3 className='title-large'>{params.title}</h3>
       <div className='content body-medium'>{params.content}</div>
