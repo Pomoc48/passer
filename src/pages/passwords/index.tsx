215169c6e357a9bc6018540a733b0e9af74adcff
Mikołaj Łukawski <lukawski.mikolaj@gmail.com>
2023-06-13 19:31:36 +0200

    Fix unused values warning

--- src/pages/passwords/index.tsx
+++ src/pages/passwords/index.tsx
@@ -23,8 +23,8 @@ export default function PasswordsPage(params: { db: Firestore }) {
   const [websites, updateWebsites] = useState<Website[]>([]);
   const [showModal, setShowModal] = useState(true);
 
-  const [showPasswordDialog, setShowPasswordDialog] = useState(false);
-  const passwordDialogRef = useRef<Website | null>(null);
+  // const [showPasswordDialog, setShowPasswordDialog] = useState(false);
+  // const passwordDialogRef = useRef<Website | null>(null);
 
   const passwordRef = useRef<HTMLInputElement | null>(null);
 
