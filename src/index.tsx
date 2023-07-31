30a0390e9194231fb80c86fd9a20c9bc3483ee4e
Mikołaj Łukawski <lukawski.mikolaj@gmail.com>
2023-07-31 16:47:49 +0200

    Export Firebase config to .env

--- src/index.tsx
+++ src/index.tsx
@@ -35,13 +35,13 @@ function onUpdate() {
 onUpdate();
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC6v6N8InH2SNyjoGnfgQ_DPmV2Xw30f4k",
-  authDomain: "pass-9f64b.firebaseapp.com",
-  projectId: "pass-9f64b",
-  storageBucket: "pass-9f64b.appspot.com",
-  messagingSenderId: "31474752244",
-  appId: "1:31474752244:web:68a51225edf4bc9786dca1",
-  measurementId: "G-S9TYFLCXWG"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
