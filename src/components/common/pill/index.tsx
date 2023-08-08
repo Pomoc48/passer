6ce2a5d371ada1b41ab6af733a6cd9c89eec66ab
Mikołaj Łukawski <lukawski.mikolaj@gmail.com>
2023-08-08 16:04:05 +0200

    Merge desktop and mobile search components into one

--- src/components/common/pill/index.tsx
+++ src/components/common/pill/index.tsx
@@ -1,11 +1,15 @@
 import { PropsWithChildren } from 'react';
 import './style.scss';
 
-export default function Pill(props: PropsWithChildren<{ onClick?: () => void }>) {
+export default function Pill(props: PropsWithChildren<{ onClick?: () => void, class?: string }>) {
   let classNames = "pill";
 
   if (props.onClick !== undefined) {
     classNames += " clickable";
+  }
+
+  if (props.class !== undefined) {
+    classNames += " " + props.class;
   }
 
   return (
--- src/components/manager/search/index.tsx
+++ src/components/manager/search/index.tsx
@@ -1,24 +1,63 @@
+import { User, getAuth } from 'firebase/auth';
 import { useSearch } from '../../../context/search';
+import Avatar from '../../common/avatar';
 import Pill from '../../common/pill';
 import './style.scss';
+import { useEmailUser } from '../../../context/user';
+import { useNavigate } from 'react-router-dom';
+import { signUserOut } from '../../../functions/auth';
 
-export default function Search() {
+export default function Search(params: { user: User }) {
+  const auth = getAuth();
   const search = useSearch();
+  const userContext = useEmailUser();
+
+  const setUser = useEmailUser().update;
+  const navigate = useNavigate();
 
   return (
-    <Pill>
-      <div className="pill-icon">
-        <span className="material-icons">search</span>
-      </div>
-      <div>
-        <input
-          value={search.value}
-          className='search-input'
-          placeholder='Search passwords…'
-          onChange={e => search.update(e.target.value)}
-        />
-      </div>
-    </Pill>
+    <>
+      <Pill class='mobile-components'>
+        <div className="pill-icon">
+          <span className="material-icons">search</span>
+        </div>
+        <div>
+          <input
+            value={search.value}
+            className='search-input mobile'
+            placeholder='Search passwords…'
+            onChange={e => search.update(e.target.value)}
+          />
+        </div>
+        <div
+          className='clickable'
+          onClick={() => signUserOut(auth, setUser, navigate)}
+        >
+          <Avatar user={params.user} />
+        </div>
+      </Pill>
+      <Pill class='desktop-components'>
+        <div className="pill-icon">
+          <span className="material-icons">search</span>
+        </div>
+        <div>
+          <input
+            value={search.value}
+            className='search-input'
+            placeholder='Search passwords…'
+            onChange={e => search.update(e.target.value)}
+          />
+        </div>
+      </Pill>
+      <Pill
+        class='desktop-components'
+        onClick={() => signUserOut(auth, userContext.update, navigate)}
+      >
+        <Avatar user={params.user} />
+        <p className='display-name'>{params.user.email!}</p>
+      </Pill>
+    </>
+
 
   );
 }
